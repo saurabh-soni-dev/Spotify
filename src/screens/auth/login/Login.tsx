e29fdf17fa3c745ea1f44d10457cef06d2946e90
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-10-28 02:23:27 +0530

    Wrap with keyboardAvoidView

--- src/screens/auth/login/Login.tsx
+++ src/screens/auth/login/Login.tsx
@@ -7,7 +7,7 @@ import {
   Header,
   TextButton,
 } from '@components/componentsIndex';
-import {View} from 'react-native';
+import {KeyboardAvoidingView, Platform, View} from 'react-native';
 import color from '@theme/color';
 
 const Login: FC = () => {
@@ -21,53 +21,57 @@ const Login: FC = () => {
   } = useLogin();
 
   return (
-    <View style={styles.container}>
-      <CustomStatusBar backgroundColor={color.backgroundDark} />
-      <Header />
-      <View style={styles.mainContainer}>
-        <CustomTextInput
-          label={`Email or username`}
-          labelStyle={styles.labelStyle}
-          value={loginInfo.email}
-          onChangeText={value => updateFieldValue('email', value)}
-          inputMode="email"
-          keyboardType="email-address"
-          returnKeyType="next"
-          error={errors.email}
-        />
-        <CustomTextInput
-          containerStyle={styles.password}
-          label={`Password`}
-          labelStyle={styles.labelStyle}
-          value={loginInfo.password}
-          onChangeText={value => updateFieldValue('password', value)}
-          secureTextEntry={true}
-          inputMode="text"
-          keyboardType="visible-password"
-          returnKeyType="next"
-          error={errors.password}
-        />
-        <TextButton
-          type="onlyText"
-          label="Log in"
-          disabled={!isLogin}
-          underlayColor={color.inactive}
-          labelStyle={{color: color.backgroundDark}}
-          buttonContainerStyle={
-            isLogin ? styles.focusLoginButton : styles.loginButton
-          }
-          onPress={onpressLogin}
-        />
-        <TextButton
-          type="onlyText"
-          label="Log in without password"
-          underlayColor={color.backgroundDark}
-          labelStyle={{color: color.white}}
-          buttonContainerStyle={styles.loginWithoutPassButton}
-          onPress={onpressLoginWithoutPassword}
-        />
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <View style={styles.container}>
+        <CustomStatusBar backgroundColor={color.backgroundDark} />
+        <Header />
+        <View style={styles.mainContainer}>
+          <CustomTextInput
+            label={`Email or username`}
+            labelStyle={styles.labelStyle}
+            value={loginInfo.email}
+            onChangeText={value => updateFieldValue('email', value)}
+            inputMode="email"
+            keyboardType="email-address"
+            returnKeyType="next"
+            error={errors.email}
+          />
+          <CustomTextInput
+            containerStyle={styles.password}
+            label={`Password`}
+            labelStyle={styles.labelStyle}
+            value={loginInfo.password}
+            onChangeText={value => updateFieldValue('password', value)}
+            secureTextEntry={true}
+            inputMode="text"
+            keyboardType="visible-password"
+            returnKeyType="next"
+            error={errors.password}
+          />
+          <TextButton
+            type="onlyText"
+            label="Log in"
+            disabled={!isLogin}
+            underlayColor={color.inactive}
+            labelStyle={{color: color.backgroundDark}}
+            buttonContainerStyle={
+              isLogin ? styles.focusLoginButton : styles.loginButton
+            }
+            onPress={onpressLogin}
+          />
+          <TextButton
+            type="onlyText"
+            label="Log in without password"
+            underlayColor={color.backgroundDark}
+            labelStyle={{color: color.white}}
+            buttonContainerStyle={styles.loginWithoutPassButton}
+            onPress={onpressLoginWithoutPassword}
+          />
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
